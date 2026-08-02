7c971e2d915e3cd3269a73cf8df238c0b86c5687
Deepti-web <deeptinirmalyamohanty@gmail.com>
2026-08-02 18:50:18 +0530

    remove tracking router from API router

--- api/api.py
+++ api/api.py
@@ -5,7 +5,6 @@ from api.commiter.router import commiter_router
 from api.labfile.router import labfile_router
 from api.notes.router import notes_router
 from api.taskmanager.router import taskmanager_router
-from api.tracking.router import tracking_router
 from api.vibelist.router import vibelist_router
 api_router = APIRouter()
 
@@ -15,5 +14,4 @@ api_router.include_router(commiter_router, prefix="/commiter", tags=["Commiter"]
 api_router.include_router(labfile_router, prefix="/labfile", tags=["Labfile"])
 api_router.include_router(notes_router, prefix="/notes", tags=["Notes"])
 api_router.include_router(taskmanager_router, prefix="/taskmanager", tags=["Taskmanager"])
-api_router.include_router(tracking_router, prefix="/tracking", tags=["Tracking"])
 api_router.include_router(vibelist_router, prefix="/vibelist", tags=["Vibelist"])
